f29a02d27ff9d0296ba939be5c87309ed8f9adbc
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-07-03 17:20:21 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "new-website",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "2cUzbNC6WcEcNry9FueO2caMeKnMbLR2-m-TLpFbkY8",
+        spaceId: "",
+      },
+    },
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-120800801-1",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
