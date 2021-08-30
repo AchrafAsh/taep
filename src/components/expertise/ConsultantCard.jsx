7db5440819afc21229c21b7210702e80b745fa9e
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-08-30 10:14:30 +0200

    add link to page

--- src/components/expertise/ConsultantCard.jsx
+++ src/components/expertise/ConsultantCard.jsx
@@ -10,12 +10,26 @@ export default function ConsultantCard({ image, name, link }) {
             <div className='bg-white w-full flex flex-col space-y-2 p-4'>
                 <span className='font-light text-blue-1000'>{name}</span>
                 <a
-                    className='text-gray-400 font-light'
+                    className='text-gray-400 hover:text-gray-600 font-light flex space-x-1'
                     href={link}
                     target='_blank'
                     rel='noreferrer noopener'
                 >
-                    Linkedin
+                    <span>Linkedin</span>
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-6 w-5'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth={1}
+                            d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
+                        />
+                    </svg>
                 </a>
             </div>
         </div>
