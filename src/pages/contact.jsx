f7fafcfd0f1e70469fd0f92adb54d7614aff8799
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-08-30 10:14:09 +0200

    responsive design

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,15 +4,17 @@ import { StaticImage } from 'gatsby-plugin-image'
 import { InlineWidget } from 'react-calendly'
 
 const ContactPage = () => (
-    <main className='h-screen flex flex-row'>
-        <section className='p-16 bg-blue-1000 text-white'>
-            <h1 className='my-20 text-4xl font-bold'>Contactez nous</h1>
+    <main className='md:h-screen flex flex-col-reverse md:flex-row'>
+        <section className='p-6 lg:p-16 bg-blue-1000'>
+            <h1 className='my-6 md:my-20 text-4xl font-bold text-white'>
+                Contactez nous
+            </h1>
 
             <div className='my-12'>
                 <h3 className='text-2xl text-yellow-300 font-semibold mb-2'>
                     Adresse
                 </h3>
-                <p className='leading-8'>
+                <p className='leading-8 text-white'>
                     828 Boulevard des Maréchaux
                     <br /> 91120 Palaiseau
                     <br /> France
@@ -23,7 +25,7 @@ const ContactPage = () => (
                 <h3 className='text-2xl text-yellow-300 font-semibold mb-2'>
                     Contact
                 </h3>
-                <p className='leading-8'>
+                <p className='leading-8 text-white'>
                     taep@ensta-paris.fr
                     <br />
                     +33 1 81 87 18 86
@@ -37,8 +39,8 @@ const ContactPage = () => (
             </div>
         </section>
 
-        <section className='flex-1 p-20 flex flex-col space-y-12 justify-between items-center overflow-y-auto'>
-            <div>
+        <section className='flex-1 py-12 lg:p-20 flex flex-col space-y-12 justify-between items-center overflow-y-auto'>
+            <div className='px-6 md:px-12'>
                 <h1 className='text-4xl font-bold text-blue-1000 mb-8'>
                     Parlez nous de votre projet
                 </h1>
@@ -78,7 +80,7 @@ const ContactPage = () => (
                 </form>
             </div>
 
-            <div className='w-full max-w-xl flex flex-row space-x-8 items-center'>
+            <div className='px-6 lg:p-0 w-full max-w-xl flex flex-row space-x-8 items-center'>
                 <hr className='bg-gray-300 flex-1' />
                 <span className='uppercase text-gray-300'>ou</span>
                 <hr className='bg-gray-300 flex-1' />
