8074d427ce7180cf419fb466d37cbfaa9f909b35
ChristopherTsa <93998019+ChristopherTsa@users.noreply.github.com>
2022-03-18 16:48:38 +0100

    Changement sur le placement description specialite

--- src/templates/expertise/index.jsx
+++ src/templates/expertise/index.jsx
@@ -245,7 +245,7 @@ const Page = ({
                                             {item.title}
                                         </h4>
                                     </div>
-                                    <div className='max-w-xs px-4'>
+                                    <div className='max-w-xs px-8'>
                                         {item.description
                                             .split('\\n')
                                             .map((item, i) => (
