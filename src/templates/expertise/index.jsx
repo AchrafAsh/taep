1704aba26358821a65bf30d3fc9e0fedc679526e
ChristopherTsa <93998019+ChristopherTsa@users.noreply.github.com>
2022-03-22 13:06:45 +0100

    Update index.jsx

--- src/templates/expertise/index.jsx
+++ src/templates/expertise/index.jsx
@@ -245,7 +245,7 @@ const Page = ({
                                             {item.title}
                                         </h4>
                                     </div>
-                                    <div className='max-w-xs px-4 py-8'>
+                                    <div className='max-w-xs px-4 pt-8'>
                                         {item.description
                                             .split('\\n')
                                             .map((item, i) => (
