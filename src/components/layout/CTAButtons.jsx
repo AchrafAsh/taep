56359ad901226e26c6954086ab50fe4641fb658b
ChristopherTsa <93998019+ChristopherTsa@users.noreply.github.com>
2022-03-22 13:15:42 +0100

    Update CTAButtons.jsx

--- src/components/layout/CTAButtons.jsx
+++ src/components/layout/CTAButtons.jsx
@@ -24,6 +24,26 @@ const Button = ({ text, svg, onClick }) => {
 export default function CTAButtons() {
     return (
         <nav className='fixed z-10 right-0 bottom-0 sm:top-1/2 text-black p-4 flex flex-col items-end space-y-4'>
+             <Button
+                svg={
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-6 w-6'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth={2}
+                            d='M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z'
+                        />
+                    </svg>
+                }
+                text='Présenter mon projet'
+                onClick={() => navigate('/contact')}
+            />
             <Button
                 svg={
                     <svg
@@ -42,26 +62,6 @@ export default function CTAButtons() {
                     </svg>
                 }
                 text='Parler avec un administrateur'
-                onClick={() => navigate('/contact')}
-            />
-            <Button
-                svg={
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        className='h-6 w-6'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
-                    >
-                        <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            strokeWidth={2}
-                            d='M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z'
-                        />
-                    </svg>
-                }
-                text='Présenter mon projet'
                 onClick={() => navigate('/contact')}
             />
         </nav>
